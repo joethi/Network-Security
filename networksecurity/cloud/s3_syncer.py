e8b26944a38bd1a298cc1f39c283fa88004b3307
joethi <Jothi.Thondiraj@colorado.edu>
2025-10-01 01:27:17 -0700

    training pipeline completed.

---
+++ networksecurity/cloud/s3_syncer.py
@@ -0,0 +1,20 @@
+
+import os
+import sys
+from networksecurity.exception.exception import NetworkSecurityException
+
+
+class S3Sync:
+    def sync_folder_to_s3(self,folder,aws_bucket_url):
+        try:
+            command = f"aws s3 sync {folder} {aws_bucket_url} "
+            os.system(command)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def sync_folder_from_s3(self,folder,aws_bucket_url):
+        try:
+            command = f"aws s3 sync  {aws_bucket_url} {folder} "
+            os.system(command)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
